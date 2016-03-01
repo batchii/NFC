50ee293cb25321dd37f53956b1aadc82df0217d0
Alec Tabatchnick <atab70782@gmail.com>
2016-03-01 11:59:47 -0500

    Card activity updates text views

--- Demo1/app/src/main/java/com/csie/nfc/demo1/CardActivity.java
+++ Demo1/app/src/main/java/com/csie/nfc/demo1/CardActivity.java
@@ -7,6 +7,7 @@ import android.nfc.NfcAdapter;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,7 +16,8 @@ import org.w3c.dom.Text;
 public class CardActivity extends AppCompatActivity {
 
     private TextView nameTextView;
-
+    private TextView phoneTextView;
+    private String nameString, phoneString;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +25,28 @@ public class CardActivity extends AppCompatActivity {
 
         // ImageView that we'll use to display cards
         nameTextView = (TextView)findViewById(R.id.nameCard);
+        phoneTextView = (TextView)findViewById(R.id.phoneNumberCard);
 
         // see if app was started from a tag and show game console
         Intent intent = getIntent();
         if(intent.getType() != null && intent.getType().equals(MimeType.NFC_DEMO)) {
             Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
             NdefMessage msg = (NdefMessage) rawMsgs[0];
-            NdefRecord cardRecord = msg.getRecords()[0];
-            String consoleName = new String(cardRecord.getPayload());
-            displayCard(consoleName);
+            Log.d("Messages" , msg.getRecords().toString());
+            NdefRecord nameRecord = msg.getRecords()[0];
+            NdefRecord phoneRecord = msg.getRecords()[2];
+            nameString = new String(nameRecord.getPayload());
+            phoneString = new String(phoneRecord.getPayload());
+            displayDetails();
         }
     }
 
-    private void displayCard(String consoleName) {
-        int cardResId = 0;
-        if(consoleName.equals("nes")) cardResId = R.drawable.nes;
-        else if(consoleName.equals("snes")) cardResId = R.drawable.snes;
-        else if(consoleName.equals("megadrive")) cardResId = R.drawable.megadrive;
-        else if(consoleName.equals("mastersystem")) cardResId = R.drawable.mastersystem;
+    private void displayDetails() {
+        if(phoneString != null && nameString != null){
+            nameTextView.setText(nameString);
+            phoneTextView.setText(phoneString);
+        } else {
+            Log.e("Something misread", "Misread");
+        }
     }
 }
