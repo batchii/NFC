f63ed995a54c6f091b7f83166e626332e93f6a6d
katiec089 <katiec089@gmail.com>
2016-04-25 13:21:10 -0400

    connected promo activity to main

--- RestaurantManager/app/src/main/java/restaurantmanager/nfc/csie/packagecom/restaurantmanager/PromotionalActivity.java
+++ RestaurantManager/app/src/main/java/restaurantmanager/nfc/csie/packagecom/restaurantmanager/PromotionalActivity.java
@@ -1,5 +1,6 @@
 package restaurantmanager.nfc.csie.packagecom.restaurantmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.EditText;
 public class PromotionalActivity extends AppCompatActivity {
 
     private String promotion = "";
+//    private Intent me = getIntent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +22,27 @@ public class PromotionalActivity extends AppCompatActivity {
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         Button b = (Button) findViewById(R.id.submit);
 
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText et = (EditText) findViewById(R.id.promotionalText);
                 String txt = et.getText().toString();
-                setPromotionString(txt);
+                System.out.println(txt);
+//                setPromotionString(txt);
+                Intent me = getIntent();
+                me.putExtra("PROMO", txt);
+                finish();
             }
         });
+
 
 
     }
 
     private void setPromotionString(String txt) {
-//        System.out.println(this.promotion);
+        System.out.println(this.promotion);
         this.promotion = txt;
     }
 
